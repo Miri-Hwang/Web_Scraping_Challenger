690b942c56f2c606e865053b1f36174f2857e9e6
Miri-Hwang <72543560+Miri-Hwang@users.noreply.github.com>
2021-02-01 15:17:22 +0900

    style : Format Changed

--- alba.py
+++ alba.py
@@ -3,25 +3,25 @@ from bs4 import BeautifulSoup
 
 URL = "http://www.alba.co.kr/"
 
-result = requests.get(URL)
-soup = BeautifulSoup(result.text, "html.parser")
-brands = soup.find("div", {"id": "MainSuperBrand"}).find_all(
-    "li", {"class": "impact"})
-
-brand_list = []
-
 
 def extract_brands(html):
     company = html.find("span", {"class": "company"}).get_text()
     company_url = html.find("a", {"class": "goodsBox-info"})["href"]
     return {'company': company, 'url': company_url}
 
+# 메인 페이지에서 슈퍼 채용 브랜드 리스트를 얻는 함수
+
 
 def get_brands():
+    result = requests.get(URL)
+    soup = BeautifulSoup(result.text, "html.parser")
+    brands = soup.find("div", {"id": "MainSuperBrand"}).find_all(
+        "li", {"class": "impact"})
+    brand_list = []
     for brand in brands:
         brand = extract_brands(brand)
         brand_list.append(brand)
     return brand_list
 
 
-print(get_brands())
+brands = get_brands()
